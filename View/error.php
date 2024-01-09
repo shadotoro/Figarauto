767a7b95f97f4fa0b29dd16af1563650715b9b91
shadotoro <132667369+shadotoro@users.noreply.github.com>
2024-01-09 18:16:40 +0100

    sémantique

--- View/error.php
+++ View/error.php
@@ -8,15 +8,20 @@ unset($_SESSION['error_message']);
 
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>Erreur</title>
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <title>Erreur</title>
+</head>
+<body>
+    <header>
+        <h1>Oups !</h1>
+    </header>
+    <main>
         <div class="error-container">
-            <h1>Oups !</h1>
             <p><?php echo htmlspecialchars($userErrorMessage); ?></p>
             <p>Si le problème persiste , contacter l'administrateur du site.</p>
             <a href="home.phtml">Retour à l'accueil</a>
         </div>
-    </body>
+    </main>
+</body>
+</html>
